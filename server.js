b29b592c6074dc12e6e7cfc22f6e72337b4e6b71
Sean <seantully00@hotmail.com>
2016-07-11 02:39:39 +0000

    latest commit

--- server.js
+++ server.js
@@ -2,3 +2,19 @@
 
 var express = require('express');
 var app = express();
+var url = require('url');
+var port = process.env.PORT || 8080;  
+
+app.get('*', function(req, res) {
+    var ip = req.headers['x-forwarded-for'];
+    var language = req.headers["accept-language"];
+    //var software =
+    var user = {"ip": ip, "language": language};
+	res.write(JSON.stringify(user));
+	res.end();
+    
+});
+
+app.listen(port, function () {
+  console.log('App listening on port 8080!');
+});
